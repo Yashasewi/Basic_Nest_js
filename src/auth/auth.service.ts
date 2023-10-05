7eb453d335d91707d0d71d5614db6c92e917b783
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-10-05 15:56:50 +0530

    added the try catch error in sign in

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 // import { User, Bookmark } from '@prisma/client';
 import * as argon from 'argon2';
 import { AuthDto } from './dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
 
     async signUp(dto: AuthDto) {
-        const hash = await argon.hash(dto.password);
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
-            },
-        });
-        return user;
+        try {
+            const hash = await argon.hash(dto.password);
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException(
+                        'email exist and credential taken ',
+                    );
+                }
+            }
+            throw error;
+        }
     }
 
     signIn() {
